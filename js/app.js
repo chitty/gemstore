6918970d2646e47a14af3fb72b4880cc863bffae
Carlos Chitty <chitty.carlosj@gmail.com>
2016-06-09 13:13:36 -0400

    Using Angular.js directives

--- js/app.js
+++ js/app.js
@@ -1,56 +1,163 @@
-(function(){
-	var app = angular.module('store', []);
+(function() {
+  var app = angular.module('gemStore', []);
 
-	app.controller('StoreController', function(){
-		this.products = gems;
-	});
+  /*
+   * CONTROLLERS
+   */
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
 
-	app.controller('PanelController', function(){
-		this.tab = 1;
+  app.controller("ReviewController", function(){
 
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		};
+    this.review = {};
 
-		this.isSelected = function(checkTab) {
-			return this.tab === checkTab;
-		};
-	});
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
 
-	app.controller("ReviewController", function(){
-		this.review = {};
+  });
 
-		this.addReview = function(product) {
-			product.reviews.push(this.review);
-			this.review = {};
-		};
-	});
+  /*
+   * DIRECTIVES
+   */
+  app.directive("productTitle", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-title.html"
+    };
+  });
 
-	var gems = [
-	{
-		name: 'Dodecahedron',
-		price: 2,
-		description: 'This is the best Dodecahedron in the world.',
-		reviews: [
-			{
-				stars: 5,
-				body: "I love it!",
-				author: "talover@aol.com"
-			},
-			{
-				stars: 1,
-				body: "I hate it!",
-				author: "hater@aol.com"
-			}
-		],
-		canPurchase: true,
-	},
-	{
-		name: 'Pentagonal Gem',
-		price: 5.95,
-		description: 'This is the best Pentagonal Gem in the world.',
-		canPurchase: false,
-		soldOut: true
-	}
-	];
+  app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: function(){
+         this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+
+  app.directive("productTabs", function() {
+    return {
+      restrict: "E",
+      templateUrl: "product-tabs.html",
+      controller: function() {
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(activeTab) {
+          this.tab = activeTab;
+        };
+      },
+      controllerAs: "tab"
+    };
+  });
+
+  app.directive("productDescriptions", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-description.html"
+    };
+  });
+
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict:"A",
+      templateUrl: "product-specs.html"
+    };
+  });
+
+
+  /*
+   * DATA
+   */
+  var gems = [
+    {
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        "images/azurite-1.jpg",
+        "images/azurite-2.jpg",
+        "images/azurite-3.jpg"
+      ],
+      reviews: [{
+        stars: 5,
+        body: "I love this gem!",
+        author: "joe@example.org"
+      }, {
+        stars: 1,
+        body: "This gem sucks.",
+        author: "tim@example.org"
+      }]
+    }, {
+      name: 'Shroomstone',
+      description: "Origin of the Shroomstone is in a patagonic tree, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        "images/shrooms-1.jpg",
+        "images/shrooms-2.jpg",
+        "images/shrooms-3.jpg"
+      ],
+      reviews: [{
+        stars: 3,
+        body: "I think this gem was just OK, could honestly use more shine, IMO.",
+        author: "JimmyDean@example.org"
+      }, {
+        stars: 4,
+        body: "Any gem with 12 faces is for me!",
+        author: "gemsRock@example.org"
+      }]
+      }, {
+        name: 'Sushi stone',
+        description: "Sushi stone is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/sushi-1.jpg",
+          "images/sushi-2.jpg",
+          "images/sushi-3.jpg"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org"
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org"
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org"
+        }]
+    }
+  ];
 })();
