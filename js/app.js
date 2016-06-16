433a1103a4ce0be58f3b24f567cf99486415b9f8
Carlos Chitty <chitty.carlosj@gmail.com>
2016-06-16 17:19:10 -0400

    Separated products logic into the module products.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-products']);
 
   /*
    * CONTROLLERS
@@ -17,70 +17,6 @@
       this.review = {};
     };
 
-  });
-
-  /*
-   * DIRECTIVES
-   */
-  app.directive("productTitle", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-title.html"
-    };
-  });
-
-  app.directive('productGallery', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-gallery.html',
-      controller: function(){
-         this.current = 0;
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.directive("productTabs", function() {
-    return {
-      restrict: "E",
-      templateUrl: "product-tabs.html",
-      controller: function() {
-        this.tab = 1;
-
-        this.isSet = function(checkTab) {
-          return this.tab === checkTab;
-        };
-
-        this.setTab = function(activeTab) {
-          this.tab = activeTab;
-        };
-      },
-      controllerAs: "tab"
-    };
-  });
-
-  app.directive("productDescriptions", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-description.html"
-    };
-  });
-
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
-
-  app.directive("productSpecs", function() {
-    return {
-      restrict:"A",
-      templateUrl: "product-specs.html"
-    };
   });
 
 
