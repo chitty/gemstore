ff41638781ffea5a5018bdf86ae4090efc1e0ec4
Carlos Chitty <chitty.carlosj@gmail.com>
2016-06-16 18:33:14 -0400

    Use Angular.js built in service  to get the products data from an API.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 (function() {
   var app = angular.module('gemStore', ['store-products']);
 
-  /*
-   * CONTROLLERS
-   */
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
+  app.controller('StoreController', [ '$http', function($http) {
+    var store = this;
+
+    store.products = [];
+
+    $http.get('/products.json').success(function(data){
+      store.products = data;
+    });
+  }]);
 
   app.controller("ReviewController", function(){
 
@@ -19,81 +22,4 @@
 
   });
 
-
-  /*
-   * DATA
-   */
-  var gems = [
-    {
-      name: 'Azurite',
-      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-      shine: 8,
-      price: 110.50,
-      rarity: 7,
-      color: '#CCC',
-      faces: 14,
-      images: [
-        "images/azurite-1.jpg",
-        "images/azurite-2.jpg",
-        "images/azurite-3.jpg"
-      ],
-      reviews: [{
-        stars: 5,
-        body: "I love this gem!",
-        author: "joe@example.org"
-      }, {
-        stars: 1,
-        body: "This gem sucks.",
-        author: "tim@example.org"
-      }]
-    }, {
-      name: 'Shroomstone',
-      description: "Origin of the Shroomstone is in a patagonic tree, hence its low value. It has a very high shine and 12 sides, however.",
-      shine: 9,
-      price: 22.90,
-      rarity: 6,
-      color: '#EEE',
-      faces: 12,
-      images: [
-        "images/shrooms-1.jpg",
-        "images/shrooms-2.jpg",
-        "images/shrooms-3.jpg"
-      ],
-      reviews: [{
-        stars: 3,
-        body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org"
-      }, {
-        stars: 4,
-        body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org"
-      }]
-      }, {
-        name: 'Sushi stone',
-        description: "Sushi stone is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-        shine: 70,
-        price: 1100,
-        rarity: 2,
-        color: '#000',
-        faces: 6,
-        images: [
-          "images/sushi-1.jpg",
-          "images/sushi-2.jpg",
-          "images/sushi-3.jpg"
-        ],
-        reviews: [{
-          stars: 1,
-          body: "This gem is WAY too expensive for its rarity value.",
-          author: "turtleguyy@example.org"
-        }, {
-          stars: 1,
-          body: "BBW: High Shine != High Quality.",
-          author: "LouisW407@example.org"
-        }, {
-          stars: 1,
-          body: "Don't waste your rubles!",
-          author: "nat@example.org"
-        }]
-    }
-  ];
 })();
